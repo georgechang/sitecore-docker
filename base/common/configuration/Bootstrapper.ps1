00af2e1562efd8ce0bc2ccbc9f7a22a0ee8f5b79
George Chang <george.chang@perficient.com>
2019-09-23 09:26:31 -0400

    Updates for certificates

--- base/common/configuration/Bootstrapper.ps1
+++ base/common/configuration/Bootstrapper.ps1
@@ -1,6 +1,7 @@
 param(
 	[string]$Path,
 	[string]$AppPath,
+	[string]$LicensePath,
 	[string]$Expression
 )
 
@@ -9,6 +10,10 @@ if ($Path) {
 	Import-Module .\Utilities.psm1
 	Sync-Configuration -Path $Path -AppPath $AppPath
 	Pop-Location
+}
+
+if ($LicensePath) {
+	Copy-Item -Path "/license/license.xml" -Destination $LicensePath
 }
 
 if ($Expression) {
--- base/common/configuration/Utilities.psm1
+++ base/common/configuration/Utilities.psm1
@@ -1,25 +1,28 @@
-function Sync-Configuration
-{
+function Sync-Configuration {
 	param(
 		[string]$Path,
 		[string]$AppPath
 	)
 
 	$json = Get-Content $Path | ConvertFrom-Json
-	foreach ($config in $json.configuration)
-	{
+	foreach ($config in $json.configuration) {
 		$path = Join-Path $AppPath -ChildPath $config.path | Resolve-Path
 		$config.mappings | % { Update-ConfigurationFile -Path $path -XPath $_.xpath -Property $_.property -Value $ExecutionContext.InvokeCommand.ExpandString($_.value)  }
 	}
 
-	# foreach ($certificate in $json.certificates)
-	# {
-	# 	#$path = Resolve-Path $certificate.path
-	# 	Invoke-Expression -Command ".\certoc.exe -ImportPFX -p $($certificate.secret) root $path"
-	# }
+	foreach ($certificate in $json.certificates) {
+		$path = Resolve-Path $certificate.path
+		if ([System.IO.Path]::GetExtension($path) -eq ".pfx") {
+			$secret = ConvertTo-SecureString -String $certificate.secret -AsPlainText -Force
+			$cert = Import-PfxCertificate -FilePath $path -CertStoreLocation $certificate.store -Password $secret
+			Add-CertificatePermission -Certificate $cert -User (New-Object System.Security.Principal.NTAccount("IIS APPPOOL\DefaultAppPool"))
+		}
+		else {
+			Import-Certificate -FilePath $path -CertStoreLocation $certificate.store | Out-Null
+		}
+	}
 }
-function Update-ConfigurationFile
-{
+function Update-ConfigurationFile {
 	param(
 		[string]$Path,
 		[string]$XPath,
@@ -29,6 +32,26 @@ function Update-ConfigurationFile
 
 	[xml]$XmlDocument = Get-Content -Path $Path
 	$Target = $XmlDocument.SelectSingleNode($XPath)
-	$Target.($Property) = $Value
+	if  ($Value -eq "$$DELETE$$") {
+		$Target.ParentNode.RemoveChild($Target)
+	}
+	else {
+		$Target.($Property) = $Value
+	}
 	$XmlDocument.Save($Path)
+}
+
+function Add-CertificatePermission {
+	param(
+		[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
+		[System.Security.Principal.NTAccount]$User
+	)
+
+	$rsa = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($Certificate)
+	$keyPath = "$($env:ALLUSERSPROFILE)\Microsoft\Crypto\Keys\$($rsa.key.UniqueName)"
+	$acl = Get-Acl $keyPath
+
+	$acr = New-Object System.Security.AccessControl.FileSystemAccessRule($User, 'Read', 'None', 'None', 'Allow')
+	$acl.AddAccessRule($acr)
+	Set-Acl -Path $keyPath -AclObject $acl
 }
