8be96451cb52ef70bf7365d8f1d95d37e634c1fa
George Chang <changg@gmail.com>
2019-10-06 21:20:26 -0400

    Update README and updates for version-specific labels

--- Build-ScDockerImages.ps1
+++ Build-ScDockerImages.ps1
@@ -1,5 +1,8 @@
+$winversion = "1903"
+$scversion = "9.2"
+
 # build base images
-Get-ChildItem .\base\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "scbase:$($_.Name)", "-f", ".\base\$($_.Name)\Dockerfile", "." -NoNewWindow -Wait }
+Get-ChildItem .\base\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "scbase-$($_.Name):$winversion", "--build-arg", "WINVERSION=$winversion", "-f", ".\base\$($_.Name)\Dockerfile", "." -NoNewWindow -Wait }
 
 # build application images
-Get-ChildItem .\application\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "$($_.Name):latest", "-f", ".\application\$($_.Name)\Dockerfile", "."  -NoNewWindow -Wait }
+Get-ChildItem .\application\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "$($_.Name):$winversion-$scversion", "--build-arg", "WINVERSION=$winversion", "--build-arg", "SCVERSION=$scversion", "-f", ".\application\$($_.Name)\Dockerfile", "."  -NoNewWindow -Wait }
