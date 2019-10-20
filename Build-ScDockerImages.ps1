4fe630565c3d13c90c6e1a1a4486c484813db31a
George Chang <george.chang@perficient.com>
2019-10-19 22:07:07 -0400

    Undo comments

--- Build-ScDockerImages.ps1
+++ Build-ScDockerImages.ps1
@@ -6,14 +6,14 @@ $registry="george.azurecr.io"
 $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
 
 # build dependency images
-#Start-Process docker -ArgumentList "build", "--rm", "-t", "solr:$solrversion-$winversion", "-t", "$registry/solr:$solrversion-$winversion", "--build-arg", "SOLRVERSION=$solrversion", "--build-arg", "WINVERSION=$winversion", "-f", ".\dependencies\solr\Dockerfile", "." -NoNewWindow -Wait
+Start-Process docker -ArgumentList "build", "--rm", "-t", "solr:$solrversion-$winversion", "-t", "$registry/solr:$solrversion-$winversion", "--build-arg", "SOLRVERSION=$solrversion", "--build-arg", "WINVERSION=$winversion", "-f", ".\dependencies\solr\Dockerfile", "." -NoNewWindow -Wait
 Start-Process docker -ArgumentList "build", "--rm", "-t", "mssql:2017-$winversion", "-t", "$registry/mssql:2017-$winversion", "--build-arg", "WINVERSION=$winversion", "-f", ".\dependencies\mssql\Dockerfile", "." -NoNewWindow -Wait
 
 # build base images
-#Get-ChildItem .\base\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "scbase-$($_.Name):$winversion", "--build-arg", "WINVERSION=$winversion", "-f", ".\base\$($_.Name)\Dockerfile", "." -NoNewWindow -Wait }
+Get-ChildItem .\base\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "scbase-$($_.Name):$winversion", "--build-arg", "WINVERSION=$winversion", "-f", ".\base\$($_.Name)\Dockerfile", "." -NoNewWindow -Wait }
 
 # build application images
-#Get-ChildItem .\application\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "$($_.Name):$scversion-$winversion", "-t", "$registry/$($_.Name):$scversion-$winversion", "--build-arg", "WINVERSION=$winversion", "--build-arg", "SCVERSION=$scversion", "-f", ".\application\$($_.Name)\Dockerfile", "."  -NoNewWindow -Wait }
+Get-ChildItem .\application\ -Directory | Where-Object { Test-Path "$($_.FullName)\Dockerfile" } | ForEach-Object { Start-Process docker -ArgumentList "build", "--rm", "-t", "$($_.Name):$scversion-$winversion", "-t", "$registry/$($_.Name):$scversion-$winversion", "--build-arg", "WINVERSION=$winversion", "--build-arg", "SCVERSION=$scversion", "-f", ".\application\$($_.Name)\Dockerfile", "."  -NoNewWindow -Wait }
 
 $stopwatch.Stop()
 Write-Host "Completed in $($stopwatch.Elapsed)"
