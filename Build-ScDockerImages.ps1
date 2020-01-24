4bfcab8baf087277046e2456af2e8f695c68d8c4
George Chang <george.chang@perficient.com>
2020-01-23 22:33:35 -0500

    Add traefik image

--- Build-ScDockerImages.ps1
+++ Build-ScDockerImages.ps1
@@ -63,12 +63,13 @@ if ($Dependencies) {
   Start-Process docker -ArgumentList $sqlBuildArgs -NoNewWindow -Wait
   Pop-Location
 
-  $TraefikVersion = "2.1.2"
+  $TraefikVersion = "1.7.20"
   $traefikTag = "traefik:$WindowsVersion"
   $traefikBuildArgs = $buildArgs
   $traefikBuildArgs += "-t $traefikTag"
   $traefikBuildArgs += "-t $Registry/$traefikTag"
   $traefikBuildArgs += "--build-arg WIN_VERSION=$WindowsVersion"
+  $traefikBuildArgs += "--build-arg SC_VERSION=$SitecoreVersion"
   $traefikBuildArgs += "--build-arg TRAEFIK_VERSION=$TraefikVersion"
   $traefikBuildArgs += "."
   Push-Location .\dependencies\traefik
